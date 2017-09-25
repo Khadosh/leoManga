329c35cf42548f6e377ced1e873162125c7a148c
Joaquín Nader <joaquinnader@gmail.com>
2017-09-25 13:07:10 -0300

    React Holy Grail Layout

--- src/components/about/AboutPage.js
+++ src/components/about/AboutPage.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './about-page.css';
 
 // Since this component is simple and static, there's no parent container for it.
 const AboutPage = () => {
@@ -9,6 +8,85 @@ const AboutPage = () => {
       <h1>About</h1>
       <p> This is the very first version for LeoManga </p>
       <p> This is an example of a bad link. Click <Link to="/badlink">here</Link> to see the 404 page. </p>
+      <h2> Test very large content </h2>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
+      <p>testing large content</p>
     </div>
   );
 };
