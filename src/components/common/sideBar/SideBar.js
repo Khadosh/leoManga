28f1dc05165dd9f57193a8715bc0b4e571118a8f
Joaquín Nader <joaquinnader@gmail.com>
2017-09-25 20:36:34 -0300

    populate sidebar

--- src/components/common/sideBar/SideBar.js
+++ src/components/common/sideBar/SideBar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Drawer, MenuItem } from 'material-ui';
+import { Drawer, AppBar, MenuItem } from 'material-ui';
 import HeaderBar from '../header/components/headerBar/HeaderBar';
 import SentimentVerySatisfied from 'material-ui/svg-icons/social/sentiment-very-satisfied';
 import PersonAdd from 'material-ui/svg-icons/social/person-add';
@@ -9,6 +9,9 @@ const sideBarStyle = {
   backgroundColor: 'rgb(250, 250, 250)'
 };
 
+const appBarStyle = {
+  backgroundColor: '#97a9f9'
+};
 
 const SideBar = ({isOpen, browser, toogleSideBar}) => (
   <Drawer
@@ -21,8 +24,22 @@ const SideBar = ({isOpen, browser, toogleSideBar}) => (
       !browser.lg &&
       <HeaderBar onLeftIconButtonTouchTap={toogleSideBar} />
     }
-    <MenuItem leftIcon={<SentimentVerySatisfied />} onClick={toogleSideBar}>Menu Item</MenuItem>
-    <MenuItem leftIcon={<PersonAdd />} onClick={toogleSideBar}>Menu Item 2</MenuItem>
+    <AppBar style={appBarStyle} title="Continue Reading" showMenuIconButton={false}/>
+      <MenuItem leftIcon={<SentimentVerySatisfied />} onClick={toogleSideBar}>Menu Item</MenuItem>
+      <MenuItem leftIcon={<PersonAdd />} onClick={toogleSideBar}>Menu Item 2</MenuItem>
+      <MenuItem leftIcon={<SentimentVerySatisfied />} onClick={toogleSideBar}>Menu Item</MenuItem>
+      <MenuItem leftIcon={<PersonAdd />} onClick={toogleSideBar}>Menu Item 2</MenuItem>
+      <MenuItem leftIcon={<SentimentVerySatisfied />} onClick={toogleSideBar}>Menu Item</MenuItem>
+      <MenuItem leftIcon={<PersonAdd />} onClick={toogleSideBar}>Menu Item 2</MenuItem>
+      <MenuItem leftIcon={<SentimentVerySatisfied />} onClick={toogleSideBar}>Menu Item</MenuItem>
+      <MenuItem leftIcon={<PersonAdd />} onClick={toogleSideBar}>Menu Item 2</MenuItem>
+    <AppBar style={appBarStyle} title="Recomended" showMenuIconButton={false}/>
+      <MenuItem leftIcon={<SentimentVerySatisfied />} onClick={toogleSideBar}>Menu Item</MenuItem>
+      <MenuItem leftIcon={<PersonAdd />} onClick={toogleSideBar}>Menu Item 2</MenuItem>
+      <MenuItem leftIcon={<SentimentVerySatisfied />} onClick={toogleSideBar}>Menu Item</MenuItem>
+      <MenuItem leftIcon={<PersonAdd />} onClick={toogleSideBar}>Menu Item 2</MenuItem>
+      <MenuItem leftIcon={<SentimentVerySatisfied />} onClick={toogleSideBar}>Menu Item</MenuItem>
+      <MenuItem leftIcon={<PersonAdd />} onClick={toogleSideBar}>Menu Item 2</MenuItem>
   </Drawer>
 );
 
